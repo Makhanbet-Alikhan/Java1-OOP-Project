8668a579f32d5d76e2f4cabe996cdbc9ef7f9f68
Alikhan <alilya.3108@gmail.com>
2025-12-11 08:55:18 +0500

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,7 @@ import java.util.Scanner; // Import the Scanner class to read input
 public class Main {
     public static void main(String[] args) {
         // Create a Scanner object to read input from the keyboard
-        Scanner scanner = new Scanner(System.in);
-
-        // Ask the user for their name
-        System.out.print("Enter your name: ");
-
-        // Read the user's input
-        String name = scanner.nextLine();
-
         // Print a greeting message
-        System.out.println("Hello, " + name + "! Welcome to Java.");
-
-        // Close the scanner
-        scanner.close();
+        System.out.println("Hello");
     }
 }
