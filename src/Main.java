2398b62e3fa701ced89e35aef2fb1f9556b176d7
Alikhan <alilya.3108@gmail.com>
2025-12-11 08:49:00 +0500

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner; // Import the Scanner class to read input
 
+public class Main {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the keyboard
+        Scanner scanner = new Scanner(System.in);
 
+        // Ask the user for their name
+        System.out.print("Enter your name: ");
+
+        // Read the user's input
+        String name = scanner.nextLine();
+
+        // Print a greeting message
+        System.out.println("Hello, " + name + "! Welcome to Java.");
+
+        // Close the scanner
+        scanner.close();
+    }
 }
