ed7c94e829326c2a165ea76807b7face70d2bdfc
Alikhan <alilya.3108@gmail.com>
2025-12-11 08:57:24 +0500

    fifth commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-import java.util.Scanner; // Import the Scanner class to read input
-
 public class Main {
     public static void main(String[] args) {
-        // Create a Scanner object to read input from the keyboard
-        // Print a greeting message
         System.out.println("Hello");
     }
 }
