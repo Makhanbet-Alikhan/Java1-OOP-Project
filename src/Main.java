9711bcd00b6c3b68699933ae20af7fc70f84013e
Alikhan <alilya.3108@gmail.com>
2025-12-26 11:44:40 +0500

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,29 @@
 import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+        private static ArrayList<ClothingItem> clothingItems = new ArrayList<>();
+        private static ArrayList<Customer> customers = new ArrayList<>();
+        private static ArrayList<Order> orders = new ArrayList<>();
+
+        private static Scanner scanner = new Scanner(System.in);
         public static void main(String[] args){
+
                 int customer_id_counter = 0;
                 int order_id_counter = 0;
                 int cloth_id_counter = 0;
 
-                ClothingItem Item1 = new ClothingItem(cloth_id_counter += 1, 300, "T-shirt", "Silk", true, 2);
-                ClothingItem Item2 = new ClothingItem(cloth_id_counter += 1,400, "Braces", "Silk", false, 3);
-                ClothingItem Item3 = new ClothingItem(cloth_id_counter += 1,500, "Shirt", "Wool", true, 1);
+                clothingItems.add(new ClothingItem(cloth_id_counter += 1, 300, "T-shirt", "Silk", true, 2));
+                clothingItems.add(new ClothingItem(cloth_id_counter += 1,400, "Braces", "Silk", false, 3));
+                clothingItems.add(new ClothingItem(cloth_id_counter += 1,500, "Shirt", "Wool", true, 1));
 
-                Customer customer1 = new Customer("Alikhan", customer_id_counter +=1 ,20, "+77777777777");
-                Customer customer2 = new Customer("Bek", customer_id_counter +=1 ,40, "+77027777777");
+                customers.add( new Customer("Alikhan", customer_id_counter +=1 ,20, "+77777777777"));
+                customers.add(new Customer("Bek", customer_id_counter +=1 ,40, "+77027777777"));
 
-                Order Alikhan_order = new Order(customer1, order_id_counter +=1);
-                Item1.Adding_item(Alikhan_order, Item1);
-                Item1.Adding_item(Alikhan_order, Item1);
-                Item2.Adding_item(Alikhan_order, Item2);
+                orders.add(new Order(customers.get(0), order_id_counter +=1));
+                orders.add( new Order(customers.get(1), order_id_counter +=1));
 
-                Order Bek_order = new Order(customer2, order_id_counter +=1);
-                Item1.Adding_item(Bek_order, Item1);
-                Item3.Adding_item(Bek_order, Item3);
 
 
         }
