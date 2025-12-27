833c33f723778a9005c680b343ec43fc81fd0030
Alikhan <alilya.3108@gmail.com>
2025-12-27 13:21:10 +0500

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,109 @@ public class Main {
         private static ArrayList<Customer> customers = new ArrayList<>();
         private static ArrayList<Order> orders = new ArrayList<>();
 
+        private static void displayMenu() {
+                System.out.println("\n========================================");
+                System.out.println(" Clothing Stroe System");
+                System.out.println("========================================");
+                System.out.println("1. Add Clothing Item");
+                System.out.println("2. View All Items");
+                System.out.println("3. Add Customer");
+                System.out.println("4. View All Customers");
+                System.out.println("0. Exit");
+                System.out.println("========================================");
+                System.out.print("Enter your choice: ");
+        }
+
+        private static void addClothItem() {
+                System.out.println("\n--- Add CLothing Item ---");
+                // Ask for each field
+                System.out.print("Enter item material: ");
+                String material = scanner.nextLine(); // Read string
+                System.out.print("Enter price (KZT): ");
+                double price = scanner.nextDouble(); // Read double
+                scanner.nextLine(); // Consume newline
+                System.out.print("Enter category: ");
+                String category = scanner.nextLine();
+                System.out.print("Is available? (true/false): ");
+                boolean available = scanner.nextBoolean();
+                System.out.print("Amount of item: ");
+                int amount = scanner.nextInt();
+                scanner.nextLine(); // Consume newline
+                // Create new object with user's data
+                ClothingItem item = new ClothingItem(clothingItems.size()+1, price, category, material, available, amount);
+                // Add to ArrayList
+                clothingItems.add(item);
+                System.out.println("\n Menu item added successfully!");
+        }
+
+        private static void viewAllItems(){
+                if (clothingItems.isEmpty()){
+                        System.out.println("Array is empty");
+                }else{
+                for(int ind = 0; ind < clothingItems.size(); ind++){
+                        System.out.println(ind+1 + ":" + clothingItems.get(ind));
+                }}
+        }
+
+        private static void addCustomer() {
+                System.out.println("\n--- Add Customer ---");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+
+                System.out.print("Enter discount: ");
+                int discount = scanner.nextInt();
+                scanner.nextLine();
+
+                System.out.print("Enter phone Number: ");
+                String phone_number = scanner.nextLine();
+
+                Customer customer = new Customer(name, customers.size()+1, discount, phone_number);
+                customers.add(customer);
+                System.out.println("\n Customer added successfully!");
+        }
+
+        private static void viewAllCustomers(){
+                if (customers.isEmpty()){
+                        System.out.println("Array is empty");
+                }else{
+                for(int ind = 0; ind < customers.size(); ind++){
+                        System.out.println(ind+1 + ":" + customers.get(ind));
+                }}
+        }
+
+
         private static Scanner scanner = new Scanner(System.in);
         public static void main(String[] args){
+                boolean running = true;
 
-                int customer_id_counter = 0;
-                int order_id_counter = 0;
-                int cloth_id_counter = 0;
-
-                clothingItems.add(new ClothingItem(cloth_id_counter += 1, 300, "T-shirt", "Silk", true, 2));
-                clothingItems.add(new ClothingItem(cloth_id_counter += 1,400, "Braces", "Silk", false, 3));
-                clothingItems.add(new ClothingItem(cloth_id_counter += 1,500, "Shirt", "Wool", true, 1));
-
-                customers.add( new Customer("Alikhan", customer_id_counter +=1 ,20, "+77777777777"));
-                customers.add(new Customer("Bek", customer_id_counter +=1 ,40, "+77027777777"));
-
-                orders.add(new Order(customers.get(0), order_id_counter +=1));
-                orders.add( new Order(customers.get(1), order_id_counter +=1));
+                while(running){
+                        displayMenu();
+                        int choice = scanner.nextInt();
+                        scanner.nextLine();
+                        switch (choice){
+                                case 1:
+                                        addClothItem();
+                                        break;
+                                case 2:
+                                        viewAllItems();
+                                        break;
+                                case 3:
+                                        addCustomer();
+                                        break;
+                                case 4:
+                                        viewAllCustomers();
+                                        break;
+                                case 0:
+                                        running = false;
+                                        break;
+                                default: System.out.println("Wrong entered option, try again");
+                        }
+                        if (running){
+                                System.out.println("\nPress Enter to continue...");
+                                scanner.nextLine();
+                        }
+                }
+                scanner.close();
 
 
 
